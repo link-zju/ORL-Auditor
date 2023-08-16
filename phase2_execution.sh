2807ead8d36bc9a3ced455ee5328ee6b020797d1
杜林康 <jinyang@dulinkangdeMacBook-Pro.local>
2023-08-16 11:56:59 -0700

    update phase2

--- phase2_execution.sh
+++ phase2_execution.sh
@@ -51,17 +51,19 @@ datasets_and_models_dir=datasets_and_models_set1
 env_name=("LunarLanderContinuous-v2" "BipedalWalker-v3" "Ant-v2")
 all_student_model_type=("BC" "BCQ" "IQL" "TD3PlusBC")
 
-# for task_name in ${env_name[@]};
-# do
-#     for student_model_type in ${all_student_model_type[@]};
-#     do
-#     select_GPU
-#     echo $task_name $student_model_type
-#     mkdir -p "./$datasets_and_models_dir/logs/${env_name_to_expt_dataset_name[$task_name]}/data_audit/$student_model_type"
-#     touch ./$datasets_and_models_dir/logs/${env_name_to_expt_dataset_name[$task_name]}/data_audit/$student_model_type/$random_seed.txt
-#     python main.py --datasets_and_models_dir $datasets_and_models_dir --env_name $task_name --which_experiment audit_dataset  --teacher_buffer_save_path ./$datasets_and_models_dir/${env_name_to_expt_dataset_name[$task_name]}/teacher_buffer/baseline --critic_model_tag ckpt_200.pt --student_model_tag model_50000.pt --student_agent_type  $student_model_type  --num_of_audited_episode 2 --num_shadow_student $num_shadow_student --significance_level $significance_level  --trajectory_size $trajectory_size --random_seed $random_seed --cuda $gpu_id > ./$datasets_and_models_dir/logs/${env_name_to_expt_dataset_name[$task_name]}/data_audit/$student_model_type/$random_seed.txt &
-#     done
-# done
+for task_name in ${env_name[@]};
+do
+    for student_model_type in ${all_student_model_type[@]};
+    do
+    select_GPU
+    echo $task_name $student_model_type
+    mkdir -p "./$datasets_and_models_dir/logs/${env_name_to_expt_dataset_name[$task_name]}/data_audit/$student_model_type"
+    touch ./$datasets_and_models_dir/logs/${env_name_to_expt_dataset_name[$task_name]}/data_audit/$student_model_type/$random_seed.txt
+    python main.py --datasets_and_models_dir $datasets_and_models_dir --env_name $task_name --which_experiment audit_dataset  --teacher_buffer_save_path ./$datasets_and_models_dir/${env_name_to_expt_dataset_name[$task_name]}/teacher_buffer/baseline --critic_model_tag ckpt_200.pt --student_model_tag model_50000.pt --student_agent_type  $student_model_type  --num_of_audited_episode 50 --num_shadow_student $num_shadow_student --significance_level $significance_level  --trajectory_size $trajectory_size --random_seed $random_seed --cuda $gpu_id > ./$datasets_and_models_dir/logs/${env_name_to_expt_dataset_name[$task_name]}/data_audit/$student_model_type/$random_seed.txt &
+    done
+done
+
+
 forward_task_complete
 ## put the results into a table
 python ./lib_util/draw_table.py --json_data_dir result_save/$datasets_and_models_dir --num_shadow_student $num_shadow_student --significance_level $significance_level --trajectory_size $trajectory_size
